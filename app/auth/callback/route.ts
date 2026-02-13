0446927bccd819841b954d157af4e300908c5aee
Abhitej Kantreddi <akantred@gitam.in>
2026-02-13 17:24:06 +0530

    Fix supabase export naming mismatch

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,22 +1,15 @@
-import { createServerSupabaseClient } from '@/lib/supabase-server';
-
-
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
+import { createServerClient } from '@/lib/supabase-server'
+import { cookies } from 'next/headers'
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
-  const code = searchParams.get('code');
-  const next = searchParams.get('next') ?? '/';
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
 
   if (code) {
-    const supabase = createServerSupabaseClient()
-
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-
-    if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
-    }
+    const supabase = createServerClient(cookies())
+    await supabase.auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  return NextResponse.redirect(requestUrl.origin)
 }
